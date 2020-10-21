1bb8d535f753dbbfa8742a5b569024601947a780
jfdee <58911452+jfdee@users.noreply.github.com>
2020-10-21 11:03:35 +0300

    Add files via upload

---
+++ PAINT/PAINT/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace PAINT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ColorButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.ElementList = new System.Windows.Forms.ListBox();
+            this.FillingButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(110, 130);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(543, 271);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(110, 12);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(43, 23);
+            this.ClearButton.TabIndex = 2;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // ColorButton
+            // 
+            this.ColorButton.Location = new System.Drawing.Point(494, 6);
+            this.ColorButton.Name = "ColorButton";
+            this.ColorButton.Size = new System.Drawing.Size(75, 23);
+            this.ColorButton.TabIndex = 3;
+            this.ColorButton.Text = "Color";
+            this.ColorButton.UseVisualStyleBackColor = true;
+            this.ColorButton.Click += new System.EventHandler(this.ColorButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.FillingButton);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.ColorButton);
+            this.panel1.Controls.Add(this.ClearButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(326, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(56, 17);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            // 
+            // ElementList
+            // 
+            this.ElementList.FormattingEnabled = true;
+            this.ElementList.Items.AddRange(new object[] {
+            "Brush",
+            "Rectangle",
+            "Ellipse"});
+            this.ElementList.Location = new System.Drawing.Point(0, 130);
+            this.ElementList.Name = "ElementList";
+            this.ElementList.Size = new System.Drawing.Size(104, 121);
+            this.ElementList.TabIndex = 6;
+            this.ElementList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ElementList_MouseClick);
+            // 
+            // FillingButton
+            // 
+            this.FillingButton.Location = new System.Drawing.Point(196, 12);
+            this.FillingButton.Name = "FillingButton";
+            this.FillingButton.Size = new System.Drawing.Size(75, 23);
+            this.FillingButton.TabIndex = 6;
+            this.FillingButton.Text = "Filling";
+            this.FillingButton.UseVisualStyleBackColor = true;
+            this.FillingButton.Click += new System.EventHandler(this.FillingButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ElementList);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button ColorButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox ElementList;
+        private System.Windows.Forms.Button FillingButton;
+    }
+}
+
---
+++ PAINT/PAINT/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAINT
+{
+    public partial class Form1 : Form
+    {
+        Bitmap picture;
+        int x1, y1;
+        //Настройки формы
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+
+            menuFile();
+            fillListBox();
+
+            picture = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            x1 = y1 = 0;
+
+
+        }
+
+        //Заполнение листбокса с толщиной
+        public void fillListBox()
+        {
+            for(int i = 0; i < 40; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+        }
+        public void menuFile ()
+        {
+            ToolStripMenuItem fileItem = new ToolStripMenuItem("Файл");
+
+            ToolStripMenuItem openFile = new ToolStripMenuItem("Открыть");
+            openFile.Click += openFile_Click;
+
+            ToolStripMenuItem saveFile = new ToolStripMenuItem("Сохранить");
+            saveFile.Click += saveFile_Click;
+
+            ToolStripMenuItem settingsFile = new ToolStripMenuItem("Настройки");
+            settingsFile.Click += settingsFile_Click;
+
+            fileItem.DropDownItems.Add(openFile);
+            fileItem.DropDownItems.Add(saveFile);
+            fileItem.DropDownItems.Add(settingsFile);
+
+            menuStrip1.Items.Add(fileItem);
+        }
+
+        //Открытие файла
+        void openFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*"; //формат загружаемого файла
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    picture = new Bitmap(ofd.FileName);
+                    pictureBox1.Size = picture.Size;
+                    pictureBox1.Image = picture;
+                    pictureBox1.Invalidate();
+                }
+                catch
+                {
+                    DialogResult result = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        //Сохранение файла
+        void saveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfg = new SaveFileDialog();
+            sfg.Title = "Сохранить как ...";
+            sfg.OverwritePrompt = true;
+            sfg.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)" + 
+                "|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+            sfg.ShowHelp = true;
+
+            if(sfg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    picture.Save(sfg.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+
+        }
+
+        //Настройки
+        void settingsFile_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Настройки");
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        Pen pen1 = new Pen(Color.Black, 5);
+        SolidBrush brush1 = new SolidBrush(Color.Black);
+        String elementType;
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(picture);
+            if(e.Button == MouseButtons.Left)
+            {
+                using (Pen pen = new Pen(pen1.Color, pen1.Width))
+                {
+                    pen.StartCap = LineCap.Round;
+                    pen.EndCap = LineCap.Round;
+                    pen.LineJoin = LineJoin.Round;
+
+                    if(elementType == "Brush")
+                    {
+                        g.DrawLine(pen, x1, y1, e.X, e.Y);
+                    }                                
+
+                    pictureBox1.Image = picture;
+                }
+            }
+            x1 = e.X;
+            y1 = e.Y;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        //Очистка
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+            {
+                Graphics g = Graphics.FromImage(picture);
+                g.Clear(Color.White);
+                pictureBox1.Invalidate();
+            }
+        }
+
+        //Выбор толщины
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            pen1.Width = Convert.ToInt32(listBox1.SelectedItem);
+        }
+
+
+        private void ElementList_MouseClick(object sender, MouseEventArgs e)
+        {
+            elementType = Convert.ToString(ElementList.SelectedItem);
+        }
+
+
+        Point startPoint = new Point();
+        List<int> screenBuffer = new List<20>;
+        void floodFill(int w, int h, int x, int y, Color newColor, Color oldColor)
+        {
+            if (x >= 0 && x < w && y >= 0 && y < h && screenBuffer[y][x] == oldColor && screenBuffer[y][x] != newColor)
+            {
+                screenBuffer[y * w + x] = newColor; //set color before starting recursion!
+
+                floodFill(w, h, x + 1, y, newColor, oldColor);
+                floodFill(w, h, x - 1, y, newColor, oldColor);
+                floodFill(w, h, x, y + 1, newColor, oldColor);
+                floodFill(w, h, x, y - 1, newColor, oldColor);
+            }
+        }
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(elementType != "Brush")
+            {
+                startPoint = e.Location;
+            }
+            if (elementType == "Filling")
+            {
+                int w = picture.Width;
+                int h = picture.Height;
+                Color clickedColor = picture.GetPixel(e.X, e.Y);
+                floodFill(w, h, e.X, e.Y, pen1.Color ,clickedColor);
+            }
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(elementType == "Rectangle")
+            {
+                Graphics g = Graphics.FromImage(picture);
+                g.DrawRectangle(pen1, startPoint.X, startPoint.Y, e.X, e.Y);
+            }
+
+            if(elementType == "Ellipse")
+            {
+                Graphics g = Graphics.FromImage(picture);
+                g.DrawEllipse(pen1, startPoint.X, startPoint.Y, e.X, e.Y);
+            }
+            pictureBox1.Image = picture;
+        }
+
+        private void FillingButton_Click(object sender, EventArgs e)
+        {
+            elementType = "Filling";
+        }
+
+        public void SetPixel(int x, int y)
+        {
+            picture.SetPixel(x, y, pen1.Color);
+        }
+
+        public bool checkPixel(int x, int y, Color clickedColor)
+        {
+            Color current = picture.GetPixel(x, y);
+            if(clickedColor == current)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public void RecursiveFill(int x, int y, Color clickedColor)
+        {
+            picture = new Bitmap(pictureBox1.Image);
+
+            //Если дошли до границы, то выходим
+            if (checkPixel(x, y, clickedColor) == true)
+            {
+                return;
+            }
+
+            //Иначе меняем цвет пикселя на нужный
+
+            SetPixel(x, y);
+            
+            //идем в глубь рекурсии по новому алгоритму
+            /*int i=yCoordInt + 1;
+            while (i < hsv_frame.height && CompareBool(xCoordInt, i))
+            {
+                RecursiveFill(xCoordInt, i);
+                i++;
+            }
+            RecursiveFill(xCoordInt, yCoordInt - 1);
+            RecursiveFill(xCoordInt - 1, yCoordInt);
+            RecursiveFill(xCoordInt + 1, yCoordInt);*/
+
+            //Идём в глубь рекурсии
+            RecursiveFill(x, y + 1, clickedColor);
+            RecursiveFill(x, y - 1, clickedColor);
+            RecursiveFill(x - 1, y, clickedColor);
+            RecursiveFill(x + 1, y, clickedColor);
+
+        }
+
+        
+
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
+        //Выбор цвета
+        private void ColorButton_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            pen1.Color = colorDialog1.Color;
+            brush1.Color = colorDialog1.Color;
+        }
+    }
+}
